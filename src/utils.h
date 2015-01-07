c6e3aba0a2b5a5416ed47f195525be8402f038d5
orestisf <orestisf1993@gmail.com>
2015-01-07 03:25:26 +0200

    added process structure to utils.h

--- src/utils.h
+++ src/utils.h
@@ -3,3 +3,14 @@
 void init();
 int check_builtins(char *cmd);
 #endif
+
+/* a single process.  */
+typedef struct process
+{
+  struct process *next;   /* next process in pipeline */
+  char **argv;            /* for execvp */
+  pid_t pid;              /* process ID */
+  char completed;         /* true if process has completed */
+  char stopped;           /* true if process has stopped */
+  int status;             /* reported status value */
+} process;
