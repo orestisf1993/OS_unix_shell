9d9f65e413bd3e9900eed25d8b034317d227cd4d
unknown <orestisf@lib-b-141.pclabs.itc.auth.gr>
2015-01-15 10:43:29 +0200

    added documentation for builtins.c. Untested

--- src/builtins.c
+++ src/builtins.c
@@ -17,21 +17,18 @@ const builtins_struct builtins[BUILTINS_NUM] = {
 };
 
 /**
- * @brief 
- * @param thing_name 
- * @returns 
- * 
- * 
+ * @brief Prints an invalid usage message.
+ * @param thing_name usually argv[0] or function name.
+ * @returns nothing
  */
 #define PRINT_BAD_ARGS_MSG(thing_name) {printf("%s: invalid usage\n", thing_name);}
 
 extern int always_print_dead;
+
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief enable/disable always printing child death.
+ * @param argc argument count, should be 2.
+ * @param argv argv[1] should contain "on" or "off" string.
  */
 void print_dead(int argc, char** argv){
     if (argc != 2) {
@@ -53,11 +50,9 @@ void print_dead(int argc, char** argv){
 
 int save_history_to_file = 1;
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief enables history logging.
+ * @param argc unused
+ * @param argv unused
  */
 void history_on(int argc, char** argv)
 {
@@ -70,11 +65,9 @@ void history_on(int argc, char** argv)
 }
 
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief disables history logging.
+ * @param argc unused
+ * @param argv unused
  */
 void history_off(int argc, char** argv)
 {
@@ -87,11 +80,15 @@ void history_off(int argc, char** argv)
 }
 
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief used by the help command. Print help string for a builtin command.
+ * @param argc is 1 for generic help, is 2 for specific command help.
+ * @param argv empty or contains the name of the command to print help for.
+ *
+ * print_help() searches the builtins global array for the given command
+ * string. If not found, it prints an error else it prints the help string
+ * found inside the struct. When no arguments are passed it behaves like
+ * 'help help' was called by setting the code to HELP_CMD. When the code is
+ * HELP_CMD a list of all available commands is also printed.
  */
 void print_help(int argc, char** argv)
 {
@@ -114,9 +111,11 @@ void print_help(int argc, char** argv)
 }
 
 /**
- * @brief 
- * 
- * 
+ * @brief free all memory allocated by the process structs.
+ *
+ * usually called by shell_exit(). Pointer p is used to iterate through the list,
+ * pr is used to free the last element. Once p is NULL, pr frees the last element
+ * of the list.
  */
 void free_all()
 {
@@ -130,11 +129,11 @@ void free_all()
 }
 
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief free memory and call exit.
+ * @param argc If argc==2 a custom exit code was passed.
+ * @param argv when empty, the default exit code (0) is used. Else, the code bassed as an argument is used.
+ *
+ *
  */
 void shell_exit(int argc, char** argv)
 {
@@ -149,11 +148,9 @@ void shell_exit(int argc, char** argv)
 
 
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief Print a list of all active jobs. Theoritically including the foreground one.
+ * @param argc unused.
+ * @param argv unused.
  */
 void jobs_list(int argc, char** argv)
 {
@@ -163,10 +160,12 @@ void jobs_list(int argc, char** argv)
 
     for (p = head; p != NULL; p = p->next) {
         if (p->pid) {
+            /* ignore the master process */
             printf("[%d] %s",
                    p->pid,
                    (p->completed) ? "COMPLETED" : "RUNNING");
             if (p->completed) {
+                /* currently this should never happen. */
                 printf(" status: %d\n", p->status);
             } else printf("\n");
         }
@@ -174,13 +173,19 @@ void jobs_list(int argc, char** argv)
 }
 
 /**
- * @brief 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief Change the current working directory
+ * @param argc any value >1 for a specific directory, 1 for home directory.
+ * @param argv if argc>1 argv[1] and onwards contains the target directory.
+ *
+ * argv is splitted on spaces so we must reconstruct the string to get the real
+ * target directory. This is not an ideal behavior, spaces in directory names
+ * should be escaped and not passed normally, but the current input parser doesn't
+ * allow this.
+ *
+ * The total size neeeded to be allocated is calculated with strlen() and the final
+ * string is created with strcat() calls.
  */
-void change_directory(int argc, char **argv )
+void change_directory(int argc, char **argv)
 {
     if (argc == 1) {
         /* no arguments after 'cd', change directory to HOME */
@@ -189,19 +194,20 @@ void change_directory(int argc, char **argv )
         /* Merge all argv[] strings together => handle paths with spaces in them */
         /* 0 because:
          * strlen() + 1 gives an extra '1' for at argc-1 => +1 extra
-         * we must include the null terminator => -1 */
-        size_t total_size = 0;
-        char *full_dir;
+         * we must include the null terminator but strlen doesn't include it => -1 */
+        size_t total_size = 0;  /*size in bytes that needs to be allocated */
+        char *full_dir; /* final result */
         int i;
+        /* start from argv[1] and calculate the total size with strlen() */
         for (i=1; i<argc; ++i) total_size += (strlen(argv[i]) + 1) * sizeof(char);
-        full_dir = malloc(total_size);
-        strcpy(full_dir, argv[1]);
+        full_dir = malloc(total_size); /* allocate the calculated size */
+        strcpy(full_dir, argv[1]);     /* strcpy the first string */
         for (i=2; i<argc; ++i)
         {
-            strcat(full_dir, " ");
-            strcat(full_dir, argv[i]);
+            strcat(full_dir, " ");     /* use strcat() to append a space */
+            strcat(full_dir, argv[i]); /* use strcat() to append the next string */
         }
-
+        /* call the chdir() command and detect errors */
         if ((chdir(full_dir) ) == -1)
         {
             /* error in chdir */
@@ -211,14 +217,13 @@ void change_directory(int argc, char **argv )
     }
 }
 
-
 /**
- * @brief 
- * @param code 
- * @param argc 
- * @param argv 
- * 
- * 
+ * @brief call the specified builtin function.
+ * @param code the code of the target builtin function.
+ * @param argc argument count to be passed.
+ * @param argv argument vector to be passed.
+ *
+ *
  */
 void call_builtin(int code, int argc, char** argv)
 {
@@ -226,11 +231,9 @@ void call_builtin(int code, int argc, char** argv)
 }
 
 /**
- * @brief 
- * @param cmd 
- * @returns 
- * 
- * 
+ * @brief check if a string is a builtin.
+ * @param cmd the string to be checked.
+ * @returns -1 if the command was not found. The code of the builtin if the command was found.
  */
 int check_if_builtin(char *cmd)
 {
